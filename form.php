0c0ef055671c237cb3801b46f631ddb4150b118a
IntelOrange <31019594+IntelOrange@users.noreply.github.com>
2018-03-31 16:48:48 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,8 +1,8 @@
 <?php
 header('Location: https://time2rhyme.000webhostapp.com');
-$ip = $_SERVER['REMOTE_ADDR'];
+$name = $_SERVER['REMOTE_ADDR'];
 $txt = $_POST["message"];
 $fo = fopen("messages.txt", "a");
-$fw = fwrite($fo, "$ip; $txt\n");
+$fw = fwrite($fo, "$name; $txt\n");
 fclose($fo);
 ?>
