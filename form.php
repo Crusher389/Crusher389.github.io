b5ad204a44fd4c412ffd88b8f7ebd7132346c2ee
IntelOrange <31019594+IntelOrange@users.noreply.github.com>
2018-03-31 16:38:33 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,8 @@
 <?php
-$txt = $_GET["message"];
+header('Location: http://www.google.co.uk');
+$ip = $_SERVER['REMOTE_ADDR'];
+$txt = $_POST["message"];
 $fo = fopen("messages.txt", "a");
-$fw = fwrite($fo, "$txt\n");
+$fw = fwrite($fo, "$ip; $txt\n");
 fclose($fo);
 ?>
